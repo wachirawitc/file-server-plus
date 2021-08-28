ce12961c19ce656a027060270a52dd566e2d1848
wachirawit.c <wachirawit.chankaew@gmail.com>
2021-08-28 12:26:52 +0700

    Update file server extensions

---
+++ src/FileServerPlus.Mvc/Extensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.IO;
+
+namespace FileServerPlus.Mvc.Extensions
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static void UseFileServerPlus(this IApplicationBuilder app, DirectoryInfo physicalDirectory, string requestPath)
+        {
+            UseFileServerPlus(app, physicalDirectory, requestPath, false);
+        }
+
+        public static void UseFileServerPlus(this IApplicationBuilder app, DirectoryInfo physicalDirectory, string requestPath, bool enableDirectoryBrowsing)
+        {
+            if (physicalDirectory == null)
+            {
+                throw new ArgumentNullException(nameof(physicalDirectory));
+            }
+
+            if (Directory.Exists(physicalDirectory.FullName) == false)
+            {
+                throw new DirectoryNotFoundException(physicalDirectory.FullName);
+            }
+
+            if (string.IsNullOrWhiteSpace(requestPath))
+            {
+                throw new ArgumentNullException(nameof(requestPath));
+            }
+
+            var fileProvider = new PhysicalFileProvider(physicalDirectory.FullName);
+
+            app.UseFileServer(new FileServerOptions
+            {
+                FileProvider = fileProvider,
+                RequestPath = requestPath,
+                EnableDirectoryBrowsing = enableDirectoryBrowsing
+            });
+        }
+    }
+}
