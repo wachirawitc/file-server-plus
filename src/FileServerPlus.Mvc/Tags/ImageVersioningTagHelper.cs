5b5475f06b3f53f69628089ed0a85d5cc287f050
wachirawit.c <wachirawit.chankaew@gmail.com>
2021-08-28 12:47:24 +0700

    Update file server extensions

---
+++ src/FileServerPlus.Mvc/Tags/ImageVersioningTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Text.Encodings.Web;
+
+namespace FileServerPlus.Mvc.Tags
+{
+    [HtmlTargetElement("img", Attributes = AppendVersionAttributeName + "," + SrcAttributeName, TagStructure = TagStructure.WithoutEndTag)]
+    public class ImageVersioningTagHelper : UrlResolutionTagHelper
+    {
+        private const string AppendVersionAttributeName = "asp-file-server-version";
+
+        private const string SrcAttributeName = "src";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+        }
+
+        [HtmlAttributeName(AppendVersionAttributeName)]
+        public bool AppendVersion { get; set; }
+
+        public ImageVersioningTagHelper(IUrlHelperFactory urlHelperFactory, HtmlEncoder htmlEncoder) : base(urlHelperFactory, htmlEncoder)
+        {
+        }
+    }
+}
