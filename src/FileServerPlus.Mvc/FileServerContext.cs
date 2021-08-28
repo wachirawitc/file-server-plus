d0eba3beb2dc73e5a3cd82df498400cd53223154
wachirawit.c <wachirawit.chankaew@gmail.com>
2021-08-28 14:37:12 +0700

    Update file server extensions

---
+++ src/FileServerPlus.Mvc/FileServerContext.cs
@@ -0,0 +1,52 @@
+﻿using FileServerPlus.Mvc.Extensions;
+using FileServerPlus.Mvc.Interface;
+using FileServerPlus.Mvc.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileServerPlus.Mvc
+{
+    public class FileServerContext : IFileServerContext
+    {
+        public IFileInfo Get(string src)
+        {
+            var option = FileServerRegister.Instance.GetOption(src);
+            if (option == null)
+            {
+                return null;
+            }
+
+            var subPath = src
+                .Replace("~", string.Empty)
+                .Replace(option.RequestPath, string.Empty);
+
+            var fileInfo = option.FileProvider.GetFileInfo(subPath);
+
+            return fileInfo;
+        }
+
+        public string GetUrl(string src)
+        {
+            var options = FileServerRegister.Instance.GetOption(src);
+            if (options != null)
+            {
+                var context = _httpContextAccessor.HttpContext;
+                var cache = context.Resolving<IMemoryCache>();
+                var fileVersionProvider = new FileServerVersionProvider(options.RequestPath, options.FileProvider, cache);
+
+                var path = fileVersionProvider.AddFileVersionToPath(context.Request.PathBase, src);
+                return path;
+            }
+
+            return src;
+        }
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FileServerContext(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}
---
+++ src/FileServerPlus.Mvc/Interface/IFileServerContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace FileServerPlus.Mvc.Interface
+{
+    public interface IFileServerContext
+    {
+        IFileInfo Get(string src);
+
+        string GetUrl(string src);
+    }
+}
